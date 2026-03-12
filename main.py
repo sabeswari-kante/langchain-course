e6af33f3ff4f97e3116bbeb6f3157638027fbc9f
sabeswari-kante <kante.sabeswari@gmail.com>
2026-03-12 15:59:17 +0530

    Strutured outputs using pydantic models

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ from langchain_openai import ChatOpenAI
 from langchain_ollama import ChatOllama 
 # from tavily import TavilyClient # remove this line if intersted to use langchain_tavily and remove tavily client & @tool decorator and fn search  
 from langchain_tavily import TavilySearch
-
+from pydantic import BaseModel, Field
+from typing import List
 load_dotenv() 
 
 # tavily = TavilyClient()
@@ -29,7 +30,30 @@ load_dotenv()
 # llm = ChatOllama(model="llama3")
 llm = ChatOllama(model="qwen2.5")
 tools = [TavilySearch()] # [TavilySearch()] [search]
-agent = create_agent(model = llm, tools = tools)
+# agent = create_agent(model = llm, tools = tools)
+
+
+#Pydantic 
+""" Pydantic
+-  base model is going to give us a baseclass so we can inherit from, in order to define a structured data schems
+- It is going to provide functionality like data parsing and serialization and automatic type validations
+
+- field class is going to allow us to add metadata to our models attributes
+so, we can add descriptions, which is going to be helpful for llms to understand what to put in the field""" 
+
+class Source(BaseModel):
+    """ Schema for a source used by agent"""
+    url:str= Field(description= "The URL of the source")
+
+
+class AgentResponse(BaseModel):
+    """ Schema for agent response with answer and sources"""
+    answer:str = Field(description="The agent's answer to the query")
+    sources: List[Source] = Field(default_factory= list,
+    description="List of sources used to generate answer"    )
+
+
+agent = create_agent(model = llm, tools = tools, response_format=AgentResponse) 
 
 def main():
     print("Hello from langchain-course!")
